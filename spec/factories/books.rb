415fc2882fd3f3a9bfd69f949741119a5ed1ca3a
jakoko <aerisgurlie777@gmail.com>
2015-01-22 15:24:15 -0800

    wrote pending tests for Book model

--- spec/factories/books.rb
+++ spec/factories/books.rb
@@ -2,5 +2,6 @@ FactoryGirl.define do
   factory :book do
     title "To Kill a Mockingbird"
     author "Harper Lee"
+    genre "Fiction"
   end
 end
--- spec/models/book_spec.rb
+++ spec/models/book_spec.rb
@@ -2,8 +2,11 @@ require 'rails_helper'
 
 RSpec.describe Book, :type => :model do
 
-	
-
-
+  	it "has a valid factory"
+  	it "is invalid without a title"
+  	it "is invalid without an author"
+  	it "is invalid without a genre"
+  	it "is valid without year of first publication"
+  	it "is valid without a personal review"
 
 end
